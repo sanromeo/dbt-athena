32f68594a0b10e36b8fd8a679868aebf5bc20a9f
Roman Korsun <romakorsun2000@gmail.com>
2023-12-23 00:47:23 +0200

    debug

--- dbt/include/athena/macros/materializations/models/helpers/get_partition_batches.sql
+++ dbt/include/athena/macros/materializations/models/helpers/get_partition_batches.sql
@@ -14,7 +14,6 @@
 
     {%- set table = load_result('get_partitions').table -%}
     {%- set rows = table.rows -%}
-    {%- set partitions = {} -%}
     {% do log('TOTAL PARTITIONS TO PROCESS: ' ~ rows | length) %}
     {%- set partitions_batches = [] -%}
     {%- set ns = namespace(bucket_map={}, single_partition=[], bucket_column=None) -%}
@@ -29,6 +28,7 @@
                 {%- set ns.bucket_column = bucket_match[1] -%}
                 {%- set bucket_num = adapter.murmur3_hash(col, bucket_match[2] | int) -%}
                 {%- set formatted_value = adapter.format_value_for_partition(col, column_type) -%}
+                {# Aggregate values by bucket number #}
                 {% do ns.bucket_map.setdefault(bucket_num, []).append(formatted_value) %}
             {%- else -%}
                 {%- set value = adapter.format_value_for_partition(col, column_type) -%}
@@ -37,22 +37,21 @@
             {%- endif -%}
         {%- endfor -%}
 
+        {# Grouping values by bucket number #}
         {%- for bucket_num, values in ns.bucket_map.items() -%}
-            {%- set unique_values = values | unique -%}  {# Ensure values are unique #}
+            {%- set unique_values = values | unique -%}
             {%- do ns.single_partition.append(ns.bucket_column + " IN (" + unique_values | join(", ") + ")") -%}
         {%- endfor -%}
 
         {%- set single_partition_expression = ns.single_partition | join(' and ') -%}
-        {%- set batch_number = (loop.index0 / athena_partitions_limit) | int -%}
-        {% if not batch_number in partitions %}
-            {% do partitions.update({batch_number: []}) %}
-        {% endif %}
-        {%- do partitions[batch_number].append('(' + single_partition_expression + ')') -%}
-        {%- if partitions[batch_number] | length == athena_partitions_limit or loop.last -%}
-            {%- do partitions_batches.append(partitions[batch_number] | join(' or ')) -%}
+        {%- if single_partition_expression -%}
+            {%- do partitions_batches.append('(' + single_partition_expression + ')') -%}
         {%- endif -%}
     {%- endfor -%}
 
-    {{ return(partitions_batches) }}
-
+    {%- if partitions_batches -%}
+        {{ return(partitions_batches) }}
+    {%- else -%}
+        {{ return([]) }}
+    {%- endif -%}
 {%- endmacro %}
