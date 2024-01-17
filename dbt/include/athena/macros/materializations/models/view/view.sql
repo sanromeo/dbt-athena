a045a758fa03e999950c3e6c60a47c8cc5a2add6
Roman Korsun <romakorsun2000@gmail.com>
2024-01-17 12:28:16 +0200

    Add lf-tags, lf-grants for view materialization

--- dbt/include/athena/macros/materializations/models/view/view.sql
+++ dbt/include/athena/macros/materializations/models/view/view.sql
@@ -11,6 +11,15 @@
     {% do adapter.expire_glue_table_versions(target_relation, versions_to_keep, False) %}
 
     {% set target_relation = this.incorporate(type='view') %}
+
+    {% if lf_tags_config is not none %}
+      {{ adapter.add_lf_tags(target_relation, lf_tags_config) }}
+    {% endif %}
+
+    {% if lf_grants is not none %}
+      {{ adapter.apply_lf_grants(target_relation, lf_grants) }}
+    {% endif %}
+
     {% do persist_docs(target_relation, model) %}
 
     {% do return(to_return) %}
